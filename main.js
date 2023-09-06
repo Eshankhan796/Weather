a4b6a383baf9e5ea9bd8932d9b6fadaa4abe1869
khaneshan796@gmail.com <khaneshan796@gmail.com>
2023-09-06 23:14:53 +0600

    Update

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
  // Const
- const searchInput = document.querySelector('form');
+ const searchInput = document.querySelector('.search-form');
  const mainIcon = document.querySelector('.main-icon');
  const details = document.querySelector('.details');
  const temperature = document.querySelector('.temperature');
  const icon = document.querySelector('.main-icon img');
+ 
  // Getting the data for UI update
  const updateUI = (data) => {
    const { cityDetails, weather } = data;
-
-   console.log(weather);
-   console.log(cityDetails);
-
    // Time
-const dateTimeString = weather.LocalObservationDateTime;
-const dateTime = new Date(dateTimeString);
-
-// Extract the hours
-const hours = dateTime.getHours();
-const amOrPm = hours >= 12 ? "p.m." : "a.m.";
-
-// Convert hours to 12-hour format
-const hours12 = hours % 12 || 12;
-
-// Construct the human-readable format
-const humanReadableFormat = `${hours12} ${amOrPm}`;
+   const dateTimeString = weather.LocalObservationDateTime;
+   const dateTime = new Date(dateTimeString);
+   // Extract the hours
+   const hours = dateTime.getHours();
+   const amOrPm = hours >= 12 ? "p.m." : "a.m.";
+   // Convert hours to 12-hour format
+   const hours12 = hours % 12 || 12;
+   // Construct the human-readable format
+   const humanReadableFormat = `${hours12} ${amOrPm}`;
    // Icon update
    const IconSrc = `Assets/Icons/${weather.WeatherIcon}.png`;
    icon.setAttribute('src', IconSrc);
    // Day time or night time ?
    let timeSrc = weather.IsDayTime ? 'Day' : 'Night';
-
    // Temperature updateUI update 
    temperature.innerHTML = `
-      <p><span>${weather.RealFeelTemperature.Metric.Value}</span>°${weather.RealFeelTemperature.Metric.Unit}</p><p class="weather-status">${weather.WeatherText}</p>
+   <p>
+   <span>${weather.RealFeelTemperature.Metric.Value}
+   </span>°${weather.RealFeelTemperature.Metric.Unit}
+   <p style="font-size: 20px;">
+   <span style="font-size: 20px;">${weather.RealFeelTemperature.Imperial.Value}
+   </span>°${weather.RealFeelTemperature.Imperial.Unit}
+   </p>
+   </p>
+   <p class="weather-status">${weather.WeatherText}</p>
     `;
    // Other details update 
    details.innerHTML = `
@@ -44,7 +45,7 @@ const humanReadableFormat = `${hours12} ${amOrPm}`;
     <ion-icon name="water-outline"></ion-icon>
     <p>humidity: <span>${weather.RelativeHumidity}%</span></p>
     <hr>
-    <ion-icon name="compass"></ion-icon>
+    <ion-icon name="leaf"></ion-icon>
     <p>Wind: <span>${weather.Wind.Direction.English} ${weather.Wind.Speed.Metric.Value} ${weather.Wind.Speed.Metric.Unit}</span></p>
     <hr/>
     <ion-icon name="skull-outline"></ion-icon>
@@ -67,13 +68,10 @@ const humanReadableFormat = `${hours12} ${amOrPm}`;
  // User input
  searchInput.addEventListener('submit', e => {
    e.preventDefault();
-
    const city = searchInput.city.value.trim();
-
    updateCity(city)
      .then(data => updateUI(data))
      .catch(err => {
-       console.log(err);
        alert(`"${city}" City Not Found. Sorry, Something Went Worng.`);
      });
  });
