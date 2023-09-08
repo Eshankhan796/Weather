40e558a722e2df2ee74caa76619a1c4632b8405b
khaneshan796@gmail.com <khaneshan796@gmail.com>
2023-09-08 14:40:49 +0600

    full JavaScript update

--- main.js
+++ main.js
@@ -1,72 +1,72 @@
-// Constants
-const searchForm = document.querySelector('.search-form');
-const mainIcon = document.querySelector('.main-icon img');
-const temperature = document.querySelector('.temperature');
-const details = document.querySelector('.details');
+const apiKey = 'GS2zBSjaAmBysuhkPIAaUCPpdHS8XpkS';
+const SearchQuery = document.querySelector('.search-form');
+const SearchInput = document.querySelector('.search-input');
 
-// Function to update the UI with weather data
-function updateUI(cityDetails, weather) {
-  const dateTime = new Date(weather.LocalObservationDateTime);
-  const hours = dateTime.getHours() % 12 || 12;
-  const amOrPm = dateTime.getHours() >= 12 ? 'p.m.' : 'a.m.';
-  const humanReadableTime = `${hours} ${amOrPm}`;
-  const iconSrc = `Assets/Icons/${weather.WeatherIcon}.png`;
 
-  mainIcon.setAttribute('src', iconSrc);
+SearchQuery.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  const searchValue = SearchInput.value.trim();
+  const CityURL = `http://dataservice.accuweather.com/locations/v1/cities/search/?apikey=${apiKey}&q=${searchValue}`;
 
-  temperature.innerHTML = `
-    <p>
-    <span>${weather.RealFeelTemperature.Metric.Value}
-    </span>°${weather.RealFeelTemperature.Metric.Unit}
-    </p>
-    <p style="font-size: 20px;">
-    <span style="font-size: 20px;">${weather.RealFeelTemperature.Imperial.Value}
-    </span>°${weather.RealFeelTemperature.Imperial.Unit}
-    </p>
-    <p class="weather-status">${weather.WeatherText}
-    </p>
-  `;
-
-  details.innerHTML = `
-    <ion-icon name="locate-outline"></ion-icon>
-    <p>Location: <span>${cityDetails.EnglishName}/${cityDetails.Country.EnglishName}</span></p>
-    <hr>
-    <ion-icon name="time"></ion-icon>
-    <p>Time: <span>${humanReadableTime} ${weather.IsDayTime ? 'Day' : 'Night'}</span></p>
-    <hr>
-    <ion-icon name="water-outline"></ion-icon>
-    <p>Humidity: <span>${weather.RelativeHumidity}%</span></p>
-    <hr>
-    <ion-icon name="leaf"></ion-icon>
-    <p>Wind: <span>${weather.Wind.Direction.English} ${weather.Wind.Speed.Metric.Value} ${weather.Wind.Speed.Metric.Unit}</span></p>
-    <hr>
-    <ion-icon name="skull-outline"></ion-icon>
-    <p>UV Index: <span>${weather.UVIndex} (${weather.UVIndexText})</span></p>
-    <hr>
-    <ion-icon name="eye"></ion-icon>
-    <p>Visibility: <span>${weather.Visibility.Metric.Value} ${weather.Visibility.Metric.Unit}</span></p>
-  `;
-}
-
-// Function to update city and weather data
-async function updateCity(city) {
-  try {
-    const cityDetails = await getCity(city);
-    const weather = await getWeather(cityDetails.Key);
-    return { cityDetails, weather };
-  } catch (error) {
-    throw new Error(`Error: ${error.message}`);
-  }
-}
-
-// Event listener for form submission
-searchForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const city = searchForm.city.value.trim();
-  try {
-    const data = await updateCity(city);
-    updateUI(data.cityDetails, data.weather);
-  } catch (error) {
-    alert(`"${city}" City Not Found. Sorry, Something Went Wrong.`);
-  }
+  fetch(CityURL)
+    .then(res => res.json())
+    .then(data => {
+      const CityKey = data[0];
+      weatherSearch(CityKey)
+    })
+    .catch(err => {
+      alert('Sorry error: ' + err)
+    })
 });
+
+function weatherSearch(City) {
+  const weatherURL = `http://dataservice.accuweather.com/currentconditions/v1/${City.Key}?apikey=${apiKey}&details=true`
+  const weatherIcon = document.querySelector('#weather-icon');
+  const WeatherDetails = document.querySelector('.details')
+  const TemperatureInfo = document.querySelector('.temperature')
+  fetch(weatherURL)
+    .then(res => res.json())
+    .then(data => {
+      const weather = data[0];
+      console.log(weather);
+      const dateTime = new Date(weather.LocalObservationDateTime);
+      const hours = dateTime.getHours() % 12 || 12;
+      const amOrPm = dateTime.getHours() >= 12 ? 'p.m.' : 'a.m.';
+      const humanReadableTime = `${hours} ${amOrPm}`;
+      const WeatherIconSrc = `/Assets/Icons/${weather.WeatherIcon}.png`;
+      weatherIcon.setAttribute('src', WeatherIconSrc);
+      TemperatureInfo.innerHTML = `
+      <p><span>${weather.Temperature.Metric.Value}</span>°${weather.Temperature.Metric.Unit}</p>
+      <p style="font-size: 20px;">
+      <span style="font-size: 20px;">${weather.Temperature.Imperial.Value}</span>°${weather.Temperature.Imperial.Unit}</p>
+      <p class="weather-status">${weather.WeatherText}</p>
+      `;
+      WeatherDetails.innerHTML = `
+      <ion-icon name="locate-outline"></ion-icon>
+      <p>Location: <span>${City.EnglishName}/${City.Country.EnglishName}</span>
+      </p>
+      <hr>
+      <ion-icon name="time"></ion-icon>
+      <p>Time: <span>${humanReadableTime} ${weather.IsDayTime ? 'Day' : 'Night'}</span>
+      </p>
+      <hr>
+      <ion-icon name="water-outline"></ion-icon>
+      <p>humidity: <span>${weather.RelativeHumidity}&#x25;</span>
+      </p>
+      <hr>
+      <ion-icon name="leaf"></ion-icon>
+      <p>Wind: <span>${weather.Wind.Direction.English} ${weather.Wind.Speed.Metric.Value} ${weather.Wind.Speed.Metric.Unit}</span>
+      </p>
+      <hr>
+      <ion-icon name="skull-outline"></ion-icon>
+      <p>UV Index: <span>${weather.UVIndex} (${weather.UVIndexText})</span>
+      </p>
+      <hr>
+      <ion-icon name="eye"></ion-icon>
+      <p>Visibility: <span>${weather.Visibility.Metric.Value} ${weather.Visibility.Metric.Unit}</span>
+      </p>`;
+    })
+    .catch(err => {
+      alert('Sorry error: ' + err)
+    })
+};
